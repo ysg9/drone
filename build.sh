5da90c9ba204be60a3e0554c0af7a61cd1a2fdbf
yungsg <ysg@nus.edu.sg>
2023-04-07 12:12:49 +0800

    update pkgs

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ go mod edit -replace github.com/gogo/protobuf=github.com/gogo/protobuf@v1.3.2
 go mod edit -replace github.com/miekg/dns=github.com/miekg/dns@v1.1.53
 go mod edit -replace github.com/containerd/containerd=github.com/containerd/containerd@v1.6.20
 go mod edit -replace golang.org/x/text=golang.org/x/text@v0.9.0
+go mod edit -replace golang.org/x/net=golang.org/x/net@v0.9.0
+go mod edit -replace github.com/emicklei/go-restful/v3=github.com/emicklei/go-restful/v3@v3.10.2
 go mod tidy
 
 go build -o drone-server ./cmd/drone-server
@@ -20,7 +22,6 @@ go list -m all > go.list
 ls -l go.list
 
 # nancy whitelist
-#cat > .nancy-ignore <<EOF
-#CVE-2022-29153
-#EOF
+# CVE-2022-29153: not using consul
+echo CVE-2022-29153 > .nancy-ignore
 #CVE-2022-24687
